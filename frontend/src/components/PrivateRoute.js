5ebb08a904284afc7073ca0bcbf358800ab3f3ae
KhaSenpai <email@example.comtkha22357@gmail.com>
2025-10-28 20:47:25 +0700

    giao diẹn profile

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,16 +1,18 @@
+// src/components/PrivateRoute.js
+
 import { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 const PrivateRoute = () => {
-  const { user } = useContext(AuthContext);
+  const { isAuthenticated } = useContext(AuthContext); // Lấy trạng thái đăng nhập
 
-  if (!user) {
+  if (!isAuthenticated) {
     // Nếu chưa đăng nhập, chuyển về trang login
     return <Navigate to="/login" replace />;
   }
 
-  // Nếu đã đăng nhập, cho phép truy cập
+  // Nếu đã đăng nhập, cho phép hiển thị component con (ví dụ: Profile)
   return <Outlet />;
 };
 
