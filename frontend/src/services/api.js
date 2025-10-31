a0afeb0682122b848846942b5a02c4a82d5ada64
KhaSenpai <email@example.comtkha22357@gmail.com>
2025-10-31 14:45:53 +0700

    thay doi

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Đặt base URL là địa chỉ backend của bạn
 const api = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
 });
 
 /* Interceptor: Đây là một đoạn code "nghe lén"
